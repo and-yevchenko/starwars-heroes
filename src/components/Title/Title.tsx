7323f32541264a789589fcd40686da57415f304a
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-10-23 18:07:04 +0300

    #6 add title

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,10 @@
+import './Title.css';
+
+export const Title = () => {
+    return (
+        <h1 className='title'>
+            <span className='title-first'>Star Wars</span>
+            <span className='title-second'>Heroes</span>
+        </h1>
+    )
+}
