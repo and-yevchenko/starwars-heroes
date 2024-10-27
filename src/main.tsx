ba0024bd691bf225bac3a771a92d401ac14bacc9
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-10-27 16:53:23 +0200

    #1 Fix index vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import './assets/styles/variables.css';
-import App from '.';
+import App from '../api';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
