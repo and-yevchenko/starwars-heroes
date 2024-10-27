bf3f242eefa14a8ee61b0ea021b14a5d8d9fe2a5
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-10-27 17:31:27 +0200

    #1 Fix vercel

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
-import { router } from '../src/routes/router';
+import { router } from './routes/router';
 
 const queryClient = new QueryClient();
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import './assets/styles/variables.css';
-import App from '../api';
+import App from '.';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
